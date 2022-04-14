b50e015824ec40464cd5555284710ccb5af75786
Luiz M <luiz.meneghim@hotmail.com>
2022-04-13 20:00:18 -0700

    added the categories function

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-//INNITIALIZE THE GAME BOARD ON LOAD
-initBoard();
 initCatRow();
+initBoard();
+
+document.querySelector('button').addEventListener('click', buildCategories);
 
 function initCatRow() {
   let catRow = document.getElementById('category-row');
-  for (let j = 0; j < 6; j++) {
+
+  for (let i = 0; i < 6; i++) {
     let box = document.createElement('div');
     box.className = 'clue-box category-box';
     catRow.appendChild(box);
@@ -14,7 +16,7 @@ function initCatRow() {
 function initBoard() {
   let board = document.getElementById('clue-board');
 
-  //GENERATE 5 ROWS OF CLUES, THEN PLACE 6 BOXES IN EACH ROW
+  //GENERATE 5 ROWS, THEN PLACE 6 BOXES IN EACH ROW
 
   for (let i = 0; i < 5; i++) {
     let row = document.createElement('div');
@@ -24,7 +26,8 @@ function initBoard() {
     for (let j = 0; j < 6; j++) {
       let box = document.createElement('div');
       box.className = 'clue-box';
-      box.textContent = `$ ${boxValue}`;
+      box.textContent = '$' + boxValue;
+      //box.appendChild( document.createTextNode(boxValue) ) //backwards compatible
       box.addEventListener('click', getClue, false);
       row.appendChild(box);
     }
@@ -33,6 +36,73 @@ function initBoard() {
   }
 }
 
-function getClue() {
-  console.log('have a nice day');
+function randInt() {
+  return Math.floor(Math.random() * 18418 + 1);
+}
+
+function buildCategories() {
+  const fetchReq1 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq2 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq3 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq4 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq5 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const fetchReq6 = fetch(
+    `https://jservice.io/api/category?&id=${randInt()}`
+  ).then((res) => res.json());
+
+  const allData = Promise.all([
+    fetchReq1,
+    fetchReq2,
+    fetchReq3,
+    fetchReq4,
+    fetchReq5,
+    fetchReq6,
+  ]);
+
+  allData.then((res) => {
+    console.log(res);
+    catArray = res;
+    setCategory(catArray);
+  });
+}
+
+// LOAD CATEGORIES TO THE BOARD
+
+function setCategory() {
+  let element = document.getElementById('category-row');
+  let children = element.children;
+  for (let i = 0; i < children.length; i++) {
+    children[i].innerHTML = catArray[i].title;
+  }
+}
+
+function getClue(event) {
+  let child = event.currentTarget;
+  child.classList.add('clicked-box');
+  let boxValue = child.innerHTML.slice(1);
+  let parent = child.parentNode;
+  let index = Array.prototype.findIndex.call(
+    parent.children,
+    (c) => c === child
+  );
+  let cluesList = catArray[index].clues;
+  let clue = cluesList.find((obj) => {
+    return obj.value == boxValue;
+  });
+  showQuestion(clue, child, boxValue);
 }
